65e32acd41e967a9e5b3d3c56c1760f54afc42c7
Fuad <my.fuad.adnansyah@gmail.com>
2025-12-11 16:05:47 +0800

    1. Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py

---
+++ 1. Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py
@@ -0,0 +1,29 @@
+# ============================================
+#   Contoh Tipe Data Array 1D, 2D, dan 3D
+#   By: Fuad Adnansyah
+# ============================================
+
+# Array 1 Dimensi (1D)
+array_1d = [10, 20, 30, 40, 50]
+
+# Array 2 Dimensi (2D)
+array_2d = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+# Array 3 Dimensi (3D)
+array_3d = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+]
+
+print("Array 1 Dimensi (1D):", array_1d)
+print("Array 2 Dimensi (2D):", array_2d)
+print("Array 3 Dimensi (3D):", array_3d)
